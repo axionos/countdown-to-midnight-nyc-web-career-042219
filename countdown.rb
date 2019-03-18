099e0e724778fe7b9e17d798ed0863485d46a0ec
SOYEONG OH <axionoso@gmail.com>
2019-03-18 22:59:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,12 +4,17 @@ def countdown(num)
   counter = num
   while counter > 0
     puts "#{counter} SECOND(S)!"
-
     counter -= 1
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
-
+  counter = num
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    sleep 1
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
